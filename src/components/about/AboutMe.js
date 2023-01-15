47e56199a23a5f01eee7111cb7ee47e0aa368001
Mikołaj Radek <radek_mikolaj@o2.pl>
2023-01-15 11:43:43 +0100

    blank a

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -11,10 +11,20 @@ function App() {
         <h1 className="my-name">Mikołaj Radek</h1>
         <p className="what-i-do">Zajmuje się tworzeniem stron internetowych.</p>
         <div className="social">
-          <a href="x" className="github">
+          <a
+            href="https://github.com/Mikolaj-Radek"
+            className="github"
+            target="_blank"
+            rel="noreferrer"
+          >
             Github
           </a>
-          <a href="" className="linkedin">
+          <a
+            href="https://www.linkedin.com/in/miko%C5%82aj-radek-7a1150237/"
+            className="linkedin"
+            target="_blank"
+            rel="noreferrer"
+          >
             Linkedin
           </a>
         </div>
